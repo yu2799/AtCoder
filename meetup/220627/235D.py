1a39c7c23831040886dae2f2b7230e941bd79182
yu2799 <ura.yu.2799@gmail.com>
2022-06-28 10:13:15 +0900

    fix

--- meetup/220627/235D.py
+++ meetup/220627/235D.py
@@ -4,27 +4,28 @@ from collections import deque
 
 def bfs(a, n):
     dist = [-1] * 10**6
-    dist[1] = 0
-    next_visit = deque([1])
+    dist[n] = 0
+    next_visit = deque([n])
     while next_visit:
         cur = next_visit.popleft()
-        i = cur * a
-        if i <= n and dist[i] == -1:
-            dist[i] = dist[cur] + 1
-            next_visit.append(i)
+        if cur % a == 0:
+            tmp = cur // a
+            if dist[tmp] == -1:
+                dist[tmp] = dist[cur] + 1
+                next_visit.append(tmp)
+        if len(str(cur)) >= 2 and str(cur)[1] != "0":
+            tmp = int(str(cur)[1:] + str(cur)[0])
+            if dist[tmp] == -1:
+                dist[tmp] = dist[cur] + 1
+                next_visit.append(tmp)
 
-        if len(str(cur)) >= 2 and str(cur)[-1] != "0":
-            i = int(str(cur)[-1] + str(cur)[:-1])
-            if dist[i] == -1:
-                dist[i] = dist[cur] + 1
-                next_visit.append(i)
     return dist
 
 
 def main():
     input = stdin.readline
     a, n = map(int, input().split())
-    print(bfs(a, 10**6)[n])
+    print(bfs(a, n)[1])
 
 
 if __name__ == "__main__":
