fe1410c1d3d7dfee3616f3e8d050969e12cad96e
yu2799 <ura.yu.2799@gmail.com>
2022-07-03 13:27:19 +0900

    fix

--- abc/300/260/258A.py
+++ abc/300/260/258A.py
@@ -4,14 +4,9 @@ from sys import stdin
 def main():
     input = stdin.readline
     k = int(input())
-    if k < 10:
-        print(f"21:0{k}")
-    elif k < 60:
-        print(f"21:{k}")
-    elif k < 70:
-        print(f"22:0{k - 60}")
-    else:
-        print(f"22:{k - 60}")
+    h = 21 + k // 60
+    m = k % 60
+    print(f"{h}:{m:02}")
 
 
 if __name__ == "__main__":
