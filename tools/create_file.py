b7bd31ade4e985967a0e2bccbf1bd7c0ad0c2824
yu2799 <ura.yu.2799@gmail.com>
2022-07-03 13:54:43 +0900

    update

--- tools/create_file.py
+++ tools/create_file.py
@@ -2,7 +2,7 @@ from sys import argv
 from os import makedirs
 from subprocess import Popen
 from datetime import date
-from time import sleep
+from pyperclip import copy
 
 
 def input_info():
@@ -33,32 +33,47 @@ def main():
     if files == 1:
         problem = ord(cli[3].upper())
     else:
-        problem = 65
+        problem = 64 + files
 
     if name == "abc" or name == "arc":
-        dir1 = f"/{(times + 50 - 1) // 50 * 50}"
-        dir2 = f"/{(times + 10 - 1) // 10 * 10}"
+        dir1 = f"\\{(times + 50 - 1) // 50 * 50}"
+        dir2 = f"\\{(times + 10 - 1) // 10 * 10}"
     elif name == "meetup":
         today = date.today().strftime("%y%m%d")
-        dir1 = "/" + today
+        dir1 = "\\" + today
         dir2 = ""
     else:
         print("error!")
         return
 
     # フォルダを作る
-    file_path = f"./{name}{dir1}{dir2}"
+    file_path = f".\\{name}{dir1}{dir2}"
     makedirs(file_path, exist_ok=True)
 
     for _ in [0] * files:
         problem = chr(problem)
         # raw文字列にしておく
-        full_path = rf"{file_path}/{times}{problem}.py"
+        full_path = rf"{file_path}\{times}{problem}.py"
         # ファイルを作って開く
-        with open(full_path, "a"):
-            Popen(["start", full_path], shell=True)
-        sleep(1 // 2)
-        problem = ord(problem) + 1
+        try:
+            with open(full_path, "x", encoding="utf-8") as f:
+                buf = (
+                    "from sys import stdin\n\n\n"
+                    "def main():\n"
+                    "    input = stdin.readline\n\n\n"
+                    'if __name__ == "__main__":\n'
+                    "    main()\n"
+                    ""
+                )
+                f.write(buf)
+        except FileExistsError:
+            pass
+
+        Popen(["start", full_path], shell=True)
+        problem = ord(problem) - 1
+    copy(
+        f"python {full_path}",
+    )
 
 
 if __name__ == "__main__":
